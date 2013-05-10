f74783337bd86656f912bcb409b2f9167d052cc2
Tianyang Li <tmy1018@gmail.com>
2013-05-10 11:00:57 +0800

    get gene id and isoform id

--- len_est/utils/build_gene_loci_0.py
+++ len_est/utils/build_gene_loci_0.py
@@ -3,13 +3,47 @@
 
 
 import re
+from collections import namedtuple
+
+
+"""
+$start $end
+same as python convetion
+"""
+Exon = namedtuple('Exon', ['start', 'end'])
+
+
+Isoform = namedtuple('Isoform', ['exons'])
 
 
 def get_loci(gtf_file):
+    """
+    return a dict $gene_loci
+    
+        gene_loci[gene_id] = list of gene_id's isoforms
+         
+    """
     with open(gtf_file, 'r') as fin:
-        get_gene_id = re.compile(r'gene_name "(\S+)"')
+        
+        get_gene_id = re.compile(r' gene_id "(\S+)"')
+        get_isof_id = re.compile(r'transcript_id "(\S+)"')
+        
         for line in fin:
-            gene_str = line.strip().split("\t")[-1].split("; ")[3]
-            gene_id = get_gene_id.match(gene_str).group(1)
+            
+            line = line.strip().split("\t")
+            
+            if line[2] == 'exon':
+            
+                line_attrb = line[-1].split("; ")
+                
+                gene_id = get_gene_id.match(line_attrb[0]).group(1)
+                
+                isof_id = get_isof_id.match(line_attrb[1]).group(1)
+                
+                print gene_id, isof_id
+                
+                
+            
+            
     
     
