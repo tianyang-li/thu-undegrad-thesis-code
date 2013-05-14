5d798855a61c53cf83ccb8682cec3e8032f1bfad
Tianyang Li <tmy1018@gmail.com>
2013-05-14 22:17:36 +0800

    TODO: find a test statistic

--- len_est/utils/uniform_test_0.py
+++ len_est/utils/uniform_test_0.py
@@ -7,16 +7,23 @@ from __future__ import division
 from random import randint
 
 
-def unif_stat(samples):
-    #XXX: how to get this statistic??
-    pass
-
-
-def pval(samples):
+def unif_stat(samples, lower, higher):
     """
+    test statistic assuming $samples are IID samples
+    from a discrete distribution on [$lower, $upper]
+    """
+    #XXX: how to get this statistic??
+    
+
+
+def pval(samples, lower, higher):
+    """
+    give p-value for whether $samples are IID samples
+    from discrete distribution on [$lower, $higher]
+    
     $sample  sorted samples from min to max
     
-    len(sample) >= 3
+    parametric bootstrap p-value
     """
     
     # number of simulation to get p-value
@@ -26,15 +33,11 @@ def pval(samples):
     
     my_stat = unif_stat(samples)
     
-    # number of locations that can be randomly distributed
-    n_rand_loc = len(samples) - 2
-    rand_range = samples[-1] - samples[0]
-    
     for _ in xrange(SIM_NUM):
-        rand_samples = [0]
-        for _ in xrange(n_rand_loc):
-            rand_samples.append(randint(0, rand_range))
-        rand_samples.append(rand_range)
+
+        rand_samples = [randint(lower, higher) for _ in xrange(len(samples))]
+        rand_samples.sort()
+        
         if unif_stat(rand_samples) >= my_stat:
             n_extreme += 1
     
