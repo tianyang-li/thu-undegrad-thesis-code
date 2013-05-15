929472acf5d568a45ebac032b327fd20ac3cf12e
Tianyang Li <tmy1018@gmail.com>
2013-05-15 23:44:04 +0800

    TODO: select high coverage contigs

---
+++ len_est/play/test_poisson_contig_exp_len_0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+# Tianyang Li 2013
+# ty@li-tianyang.com
+
+
+from __future__ import division
+
+import getopt
+import sys
+from math import exp
+
+
+def main():
+    cov_lambda = None
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ len_est/utils/select_high_cov_frags_0.py
@@ -0,0 +1,11 @@
+# Tianyang Li 2013
+# ty@li-tianyang.com
+
+
+def expected_len(cov_param, read_len):
+    """
+    $cov_param
+        number of reads / effective isoform length
+    """
+
+
