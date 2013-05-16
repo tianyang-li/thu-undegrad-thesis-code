5a8931449b4cbe7e2b4425223863710ffc9417a8
Tianyang Li <tmy1018@gmail.com>
2013-05-16 10:58:41 +0800

    formula for expected contig length probably right

--- len_est/play/test_poisson_contig_exp_len_0.py
+++ len_est/play/test_poisson_contig_exp_len_0.py
@@ -10,10 +10,62 @@ from __future__ import division
 import getopt
 import sys
 from math import exp
+from random import random
 
+from utils.select_high_cov_frags_0 import expected_len
+
+
+def sim_len(cov_lambda, read_len):
+    prob_read = 1 - exp(-cov_lambda)
+    
+    cur_len = 0
+    cur_space = 1
+    
+    while random() >= prob_read:
+        cur_space += 1
+    while cur_space < read_len:
+        cur_len += cur_space
+        cur_space = 1
+        while random() >= prob_read:
+            cur_space += 1
+    
+    return cur_len
+    
 
 def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+                                '',
+                                ['lambda=', 'runs=', 'read-len='])
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+        
     cov_lambda = None
+    sim_runs = None
+    read_len = None
+    
+    for o, a in opts:
+        if o == '--lambda':
+            cov_lambda = float(a)
+        if o == '--runs':
+            sim_runs = int(a)
+        if o == '--read-len':
+            read_len = int(a)
+    
+    if cov_lambda == None:
+        print >> sys.stderr, "missing lambda for coverage"
+        sys.exit(1)
+    if sim_runs == None:
+        print >> sys.stderr, "missing number of runs"
+        sys.exit(1)
+    if read_len == None:
+        print >> sys.stderr, "missing read length"
+        sys.exit(1)
+        
+    print "formula: %f" % expected_len(cov_lambda, read_len)
+    print "    sim: %f" % (sum(sim_len(cov_lambda, read_len) 
+                               for _ in xrange(sim_runs)) / sim_runs)
     
 
 if __name__ == '__main__':
--- len_est/utils/select_high_cov_frags_0.py
+++ len_est/utils/select_high_cov_frags_0.py
@@ -2,10 +2,16 @@
 # ty@li-tianyang.com
 
 
+from __future__ import division
+
+from math import exp
+
+
 def expected_len(cov_param, read_len):
     """
     $cov_param
         number of reads / effective isoform length
     """
+    return (exp(cov_param * read_len) - 1) / (exp(cov_param) - 1) - read_len
 
 
